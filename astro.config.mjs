040bd86f282e7a380732faa5d3759545b2bdacf5
Fernanda Fernandes <fernanda4design@gmail.com>
2026-01-07 22:13:17 +0000

    add netlify adapter for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [tailwind()],
 });
